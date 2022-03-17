2ea2bcf712720058014a5ee6697dbc8b6ad4af66
henrydguy <henrythepromise@gmail.com>
2022-03-16 17:00:46 -0700

    function

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,23 +1,6 @@
-#include "_putchar.h"
-
-/**
- * main - Entry point
- *
- * Return: nothing, return void
- */
+#include<main.h>
 int main(void)
-
-{
-int i; /*Declaring statements*/
-char string[8] = "_putchar"; /*Arrays*/
-
-for (i = 0 ; i < 8 ; i++) /*Start for*/
-{
-_putchar(string[i]);/*display word*/
-} /*End for*/
-
-_putchar('\n');
-
-return (0);
-
+{	
+	printf("_putchar\n");
+	return(0);
 }
